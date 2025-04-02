41fdd209cb34ba71d01bc833a41a75e01e2a0fe7
SLLLLL <3317927403@qq.com>
2025-04-02 23:00:55 +0800

    1

--- 课余/示例/turtle库.py
+++ 课余/示例/turtle库.py
@@ -1,22 +1,6 @@
 import turtle
 t = turtle.Pen()
-t.pensize(3)
-
-
-def fun(color, x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.pencolor(color)
-    for i in range(4):
-        t.forward(200)
-        t.left(90)
-    t.forward(100)
-    t.circle(100)
-
-
-fun('red', 50, 50)
-fun('blue', -250, 50)
-fun('black', -250, -250)
-fun('green', 50, -250)
+t.forward(300)
+t.setheading(90)
+t.forward(300)
 turtle.done()
