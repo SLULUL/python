2af88e7038465ad1bd363f42ef4c0ee11120a614
SLLLLL <3317927403@qq.com>
2025-04-03 21:44:52 +0800

    1

--- 课余/示例/turtle库.py
+++ 课余/示例/turtle库.py
@@ -2,10 +2,10 @@ import turtle
 t = turtle.Pen()
 t.left(36)
 t.speed(0)
-for j in range(180):
+for j in range(360//6):
     for i in range(5):
         t.forward(200)
         t.left(144)
-    t.left(5)
+    t.left(6)
     t.forward(20)
 turtle.done()
