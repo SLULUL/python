ca127991194d966800bd3da519b825dbf8d8d562
SLLLLL <3317927403@qq.com>
2025-04-03 13:44:34 +0800

    1

--- 课余/示例/turtle库.py
+++ 课余/示例/turtle库.py
@@ -1,6 +1,11 @@
 import turtle
 t = turtle.Pen()
-t.forward(300)
-t.setheading(90)
-t.forward(300)
+t.left(36)
+t.speed(0)
+for j in range(180):
+    for i in range(5):
+        t.forward(200)
+        t.left(144)
+    t.left(5)
+    t.forward(20)
 turtle.done()
